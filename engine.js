29543b30ad26fd48085a32ed4f6289dc4e32886d
Matthew Zhu <matt.z496@gmail.com>
2014-03-30 02:21:11 -0400

    Fixed: took out inline JS, cleaned up code in general, renamed variables to include hyphens

--- engine.js
+++ engine.js
@@ -1,40 +1,67 @@
 // filename: engine.js
 
+document.getElementById("BACK_BUTTON").onclick = backButton;
+document.getElementById("TEAM_OPEN").onclick = teamInterface;
+document.getElementById("START_BUTTON").onclick = startButton;
+document.getElementById("STOP_BUTTON").onclick = stopButton;
+document.getElementById("PAUSE_BUTTON").onclick = pauseButton;
+document.getElementById("REDO_BUTTON").onclick = redoButton;
+
+
 var time;
 var stoptime;
-var buffer = 100;
+var buffer;
 var deltaT;
 var ticking;
 var timeout;
 
-var minbox = document.getElementById("minbox");
-var secbox = document.getElementById("secbox");
-var minnumber = document.getElementById("minnumber");
-var secnumber = document.getElementById("secnumber");
-var teamqnum;
+var minbox = get("min-box");
+var secbox = get("sec-box");
+var minnumber = get("min-number");
+var secnumber = get("sec-number");
+var teamqnum = 1;
 var teamstate = "stopped";
 // "stopped": the timer is not running at all
 // "paused": the timer is on, we do not have the option to go to the next question, but it is not running
 // "running": timer is running
 
+function get(elem)
+{ return document.getElementById(elem); }
+
 function backButton()
 {
   // since getElementByClassName doesn't play well with the code below it, we'll have to add each test style box individually...
-  document.getElementById("teambox").style.display = "none";
-  document.getElementById("buttonbox").style.display = "block";
-  document.getElementById("backbutton").style.display = "none";
+  get("team-box").style.display = "none";
+  // and pause all the timers unless they're stopped already
+  if (teamstate !== "stopped")
+  {teamstate = "paused";}
+  get("button-box").style.display = "block";
+  get("BACK_BUTTON").style.display = "none";
 }
 
 function teamInterface()
 {
-  document.getElementById("buttonbox").style.display = "none";
-  document.getElementById("teambox").style.display = "block";
-  document.getElementById("backbutton").style.display = "inline-block";
-  time = 240; //240s = 4m
-  deltaT = 1000; // actual time between increments of the time variable -- 1000 in normal situation
-  stoptime = time * deltaT; //ms
-  buffer = 100; //ms
-  teamqnum = 1;
+  get("button-box").style.display = "none";
+  get("team-box").style.display = "block";
+  get("BACK_BUTTON").style.display = "inline-block";
+  // if teamstate happens to be paused already, that's because "back to menu" was pressed while running OR while paused by user
+  // pretend we were running as usual and got paused by a mysterious force
+  // under user-paused conditions, this doesn't produce any changes
+  if (teamstate === "paused")
+  {
+    teamstate = "running";
+    pauseButton();
+  }
+  // if teamstate happens to be stopped already, it was stopped before "back to menu" OR we have just started
+  else
+  {
+    // initialize things
+    time = 240; //240s = 4m
+    deltaT = 1000; // actual time between increments of the time variable -- 1000 in normal situation
+    stoptime = time * deltaT; //ms
+    buffer = 100; //ms
+    // do NOT re-initialize teamqnum! or else redo button breaks!
+  }
 }
 
 function startTimer()
@@ -46,7 +73,7 @@ function startTimer()
   minnumber.style.color = "inherit";
   secbox.style.background = "transparent";
   secnumber.style.color = "inherit";
-  document.getElementById("redobutton").style.display = "none";
+
   teamstate = "running";
   ticking = setInterval(tick, deltaT);
   timeout = setTimeout(finish, stoptime+buffer);
@@ -60,7 +87,16 @@ function tick()
   // parse time remaining into the divs
   minnumber.innerHTML = Math.ceil((240 - time)/60);
   secnumber.innerHTML = time % 60;
-  // however, secnumber should only read 0 at the end
+    
+  // check if <15sec in the minute
+  if (time % 60 <= 15 && time % 60 !== 0)
+  {
+    // make boxes yellow w/ white text for warning
+    secbox.style.background = "#f2c01b";
+    secnumber.style.color = "#f9f6f2";
+  }
+  
+    // however, secnumber should only read 0 at the end
   if (time % 60 === 0 && time !== 0)
   {
     secnumber.innerHTML = 60;
@@ -70,53 +106,28 @@ function tick()
     secbox.style.background = "transparent";
     secnumber.style.color = "inherit";
   }
-  
-  if (time === 0)
-    secnumber.innerHTML = 0;
 
-  // check if <15sec in the minute
-  if (time % 60 <= 15 && time % 60 !== 0)
-  {
-    // make boxes yellow w/ white text for warning
-    secbox.style.background = "#f2c01b";
-    secnumber.style.color = "#f9f6f2";
-  }
-}
-  
-function finish()
-{
-  clearInterval(ticking);
-  // timeout has already fired, so no need to clear it
-  // only reset once
-  if (teamstate === "running" || teamstate === "paused")
-  {
-    // make the boxes red so they're noticeable
-    minbox.style.background = "#f72d23";
-    secbox.style.background = "#f72d23";
-    minnumber.style.color = "#f9f6f2";
-    secnumber.style.color = "#f9f6f2";
-    // let user advance
-    reset();
-    teamstate = "stopped";
-    // show the redo button
-    document.getElementById("redobutton").style.display = "inline-block";
-  }
+  /** end -- backup for this in finish()...
+  if (time === 0)
+    secnumber.innerHTML = 0; */
+
 }
 
 function startButton()
 {
-  // make the button into text that just says the question number
-  document.getElementById("startbutton").style.display = "none";
-  document.getElementById("ghostbutton").style.display = "inline-block";
+  // remake the buttons
+  get("START_BUTTON").style.display = "none";
+  get("ghost-button").style.display = "inline-block";
+  get("REDO_BUTTON").style.display = "none";
   // start the timer and let it goooo
   startTimer();
 }
   
 function stopButton()
 {
-  // premature finish (clearInterval has already fired); clear timeout
-  finish();
+  // premature finish; clear timeout, interval is cleared in the first line of finish()
   clearTimeout(timeout);
+  finish();
 }
 
 function pauseButton()
@@ -126,21 +137,56 @@ function pauseButton()
   {
     // freeze timer, don't change anything
     teamstate = "paused";
-    // remove timeout
+    // remove timeout and interval for now, to be replaced
     clearInterval(ticking);
     clearTimeout(timeout);
-    // change text to Resume
-    document.getElementById("pausebutton").innerHTML = "Resume...";
+    // change text to Resume...
+    get("PAUSE_BUTTON").innerHTML = "Resume...";
   }
   
   // paused --> running section
   else if (teamstate === "paused")
   {
     teamstate = "running";
-    // remake timeout
+    // remake timeout and interval
     ticking = setInterval(tick, deltaT);
-    timeout = setTimeout(finish, time * 1000 + buffer); // timeout after time remaining finishes up
-    document.getElementById("pausebutton").innerHTML = "Pause";
+    timeout = setTimeout(finish, time*deltaT + buffer); // timeout for the time remaining to finish up
+    // resembles definition of stoptime except that stoptime is a constant
+    get("PAUSE_BUTTON").innerHTML = "Pause";
+  }
+}
+
+function redoButton()
+{
+  // essentially from a paused position, decrease qnum by 1 so the start button reads something else, but can't go past 1
+  if (teamqnum > 1)
+  {
+    teamqnum--;
+    get("start-button-num").innerHTML = "Question " + teamqnum;
+    get("team-current-num").innerHTML = "Question " + teamqnum;
+  }
+}
+
+function finish()
+{
+  // this must be here in the case of non-user-initiated stop (OoT)
+  // by definition the timeout has already cleared, since this is the function that the timeout triggers
+  clearInterval(ticking);
+  // now only reset once
+  if (teamstate === "running" || teamstate === "paused")
+  {
+    // make the boxes red so they're noticeable
+    minbox.style.background = "#f72d23";
+    secbox.style.background = "#f72d23";
+    minnumber.style.color = "#f9f6f2";
+    secnumber.style.color = "#f9f6f2";
+    // force the time to read 0 when the boxes go red, since it doesn't seem to be working in tick()
+    secnumber.innerHTML = 0;
+    // let user advance
+    reset();
+    teamstate = "stopped";
+    // show the redo button
+    get("REDO_BUTTON").style.display = "inline-block";
   }
 }
 
@@ -151,25 +197,15 @@ function reset()
   {
     teamqnum++;
   }
-  document.getElementById("startbutton").style.display = "inline-block";
-  document.getElementById("ghostbutton").style.display = "none";
-  document.getElementById("startbuttonnum").innerHTML = "Question " + teamqnum;
-  document.getElementById("teamcurrentnum").innerHTML = "Question " + teamqnum;
-  document.getElementById("pausebutton").innerHTML = "Pause";
+  get("START_BUTTON").style.display = "inline-block";
+  get("ghost-button").style.display = "none";
+  get("start-button-num").innerHTML = "Question " + teamqnum;
+  get("team-current-num").innerHTML = "Question " + teamqnum;
+  get("PAUSE_BUTTON").innerHTML = "Pause";
   time = 240;
 }
 
-function redo()
-{
-  // essentially from a paused position, decrease qnum by 1 so the start button reads something else, but can't go past 1
-  if (teamqnum > 1)
-  {
-    teamqnum--;
-    document.getElementById("startbuttonnum").innerHTML = "Question " + teamqnum;
-    document.getElementById("teamcurrentnum").innerHTML = "Question " + teamqnum;
-    time = 240;
-  }
-}
+////////////////////////////////////////////////////////
 
 function indivInterface()
 {
