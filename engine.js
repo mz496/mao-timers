fe51667ede03f1bf1e402c83654794105ecee8d8
Matthew Zhu <matt.z496@gmail.com>
2014-04-01 22:32:43 -0400

    Minor: small error fixes according to JS Hint

--- engine.js
+++ engine.js
@@ -259,7 +259,7 @@ function BufferLoader(context, urlList, callback) {
     this.context = context;
     this.urlList = urlList;
     this.onload = callback;
-    this.bufferList = new Array();
+    this.bufferList = [];
     this.loadCount = 0;
 }
 
@@ -285,19 +285,19 @@ BufferLoader.prototype.loadBuffer = function(url, index) {
                     loader.onload(loader.bufferList);
             }    
         );
-    }
+    };
 
     request.onerror = function() {
         alert('BufferLoader: XHR error');        
-    }
+    };
 
     request.send();
-}
+};
 
 BufferLoader.prototype.load = function() {
     for (var i = 0; i < this.urlList.length; ++i)
         this.loadBuffer(this.urlList[i], i);
-}
+};
 ///////////////////////////////////////////////
 // Create sound buffers for each sound
 
